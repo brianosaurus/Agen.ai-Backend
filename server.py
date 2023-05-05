fe02c38d54a9f475718f36b27b8e37b451e73e9f
Brian Woods <me@brian.biz>
2023-05-04 18:12:33 -0600

    adds a route and fastapi

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ import os
 import openai
 import json
 import random
+from fastapi import FastAPI, Request, Response
 from jsonrpcserver import method, Result, InvalidParams, Success, dispatch, serve
+import uvicorn
 
 # Define the available constraint keywords
 constraint_keywords = ['red', 'green', 'blue', 'yellow', 'purple']
@@ -16,22 +18,32 @@ openai.organization = "org-ShRy1Ith2eKCV8KxSP1x8CZj"
 openai.api_key = os.getenv("OPENAI_API_KEY") 
 openai.engine = "DALL-E"
 
+app = FastAPI()
+
 # Define the JSON-RPC endpoint method for generating images
-@method
-def generate_image(constraintKeywords, imageConstraints, subject) -> Result:
+@app.post("/api")
+async def generate_image(request: Request):
+    request_body = await request.json()
+
+    print(request_body)
+
+    data = request_body['params']
+
+    print(data)
+
     print("generating images")
     # Parse the constraints into lists of keywords and image constraints
     # keywords, img_constraints = constraints.get('constraintKeywords', []), constraints.get('imageConstraints', [])
     # Apply the constraints to randomly generate an image
     response = openai.Image.create(
-        prompt=subject,
+        prompt=data['subject'],
         n=5,
         size="1024x1024"
     )
     # img_url = response['data'][0]['url']
     # Return the generated image URL as a JSON-RPC response
     print(response)
-    return Success(response)
+    return response
 
 @method
 def OPTIONS() -> Result:
@@ -40,4 +52,4 @@ def OPTIONS() -> Result:
 # Start the JSON-RPC server on port 5000
 
 if __name__ == "__main__":
-    serve(port=5555)
+    uvicorn.run(app, port=5555)    
